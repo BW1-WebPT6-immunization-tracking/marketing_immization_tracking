f8edbd006cbf5c0a7a42583fc24ab4fb27d299ef
Travis Little <kingtrav@gmail.com>
2019-05-15 17:58:27 -0600

    class structure for slider component started on silider.js

---
+++ components/slider/slider.js
@@ -0,0 +1,21 @@
+/* Left to right slider component */
+
+class Slider {
+    constructor(items) {
+        this.items = items;
+        this.forwardArrow;
+        this.previousArrow;
+    }
+
+    slide (direction) {
+
+    }
+}
+
+class SliderItem {
+    constructor(item) {
+        this.item = item;
+    }
+}
+
+
