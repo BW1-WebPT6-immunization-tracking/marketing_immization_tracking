505c7368b1be1454b1e3f5309b8fa99e831f4014
Travis Little <kingtrav@gmail.com>
2019-05-15 18:17:17 -0600

    commiting before pulling html and css from master

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -5,6 +5,12 @@ class Slider {
         this.items = items;
         this.forwardArrow;
         this.previousArrow;
+        this.data;
+
+        this.items.forEach(item => {
+            return new SliderItem(item);
+        });
+        console.log('slider created', items)
     }
 
     slide (direction) {
@@ -15,7 +21,13 @@ class Slider {
 class SliderItem {
     constructor(item) {
         this.item = item;
+        console.log('slider item created');
     }
 }
+
+
+const items = document.querySelectorAll('.slider-slide');
+const slider = new Slider(items);
+
 
 
